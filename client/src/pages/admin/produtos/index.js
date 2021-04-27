f46321c8028271d953c55dc6b74ff6e5789b2de8
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-04-27 14:10:49 -0300

    exibindo dados do banco de dados

--- client/src/pages/admin/produtos/index.js
+++ client/src/pages/admin/produtos/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import AppsIcon from '@material-ui/icons/Apps';
@@ -13,13 +13,14 @@ import Typography from '@material-ui/core/Typography';
 import { makeStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
 import Link from '@material-ui/core/Link';
+import api from '../../../services/api';
 
 function Copyright() {
   return (
     <Typography variant="body2" color="textSecondary" align="center">
       {'Copyright © '}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
+      <Link color="inherit" href="/">
+        jwmffreitas
       </Link>{' '}
       {new Date().getFullYear()}
       {'.'}
@@ -64,6 +65,17 @@ const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 export default function ProdutosListagem() {
   const classes = useStyles();
 
+  const [produtos, setProdutos] = useState([])
+
+  useEffect(() => {
+    async function loadProdutos() {
+      const response = await api.get('/api/produtos')
+      console.log(response)
+      setProdutos(response.data)
+    }
+    loadProdutos()
+  }, [])
+
   return (
     <React.Fragment>
       <CssBaseline />
@@ -80,7 +92,7 @@ export default function ProdutosListagem() {
         <div className={classes.heroContent}>
           <Container maxWidth="sm">
             <Typography component="h1" variant="h2" align="center" color="textPrimary" gutterBottom>
-              Listagem de produtos
+              Lista de produtos
             </Typography>
             <Typography variant="h5" align="center" color="textSecondary" paragraph>
               As imagens são aleatórias porque e eu não cadastro imagens no DB e um card sem imagem fica feio
@@ -104,24 +116,24 @@ export default function ProdutosListagem() {
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
+            {produtos.map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card className={classes.card}>
-                  <CardMedia
+                  {/*<CardMedia
                     className={classes.cardMedia}
                     image="https://source.unsplash.com/random"
                     title="Image title"
-                  />
+                  />*/}
                   <CardContent className={classes.cardContent}>
                     <Typography gutterBottom variant="h5" component="h2">
-                      Nome Produto
+                      {card.nome}
                     </Typography>
                     <Typography>
-                      Campo designado para a descrição do produto.
+                      {card.descricao}
                     </Typography>
                     <Grid style={{textAlign: "right", marginTop: "5px"}}>
                       <Typography variant="p">
-                          Quantidade: 05
+                          Quantidade: {card.quantidade}
                       </Typography>
                     </Grid>
                   </CardContent>
@@ -134,7 +146,7 @@ export default function ProdutosListagem() {
                     </Button>
                     <Grid style={{width: "100%", textAlign: "center"}}>
                       <Typography variant="h6">
-                        R$ 10
+                        R$ {card.preco}
                       </Typography>
                     </Grid>
                   </CardActions>
@@ -147,10 +159,10 @@ export default function ProdutosListagem() {
       {/* Footer */}
       <footer className={classes.footer}>
         <Typography variant="h6" align="center" gutterBottom>
-          Footer
+          Controle de estoque
         </Typography>
         <Typography variant="subtitle1" align="center" color="textSecondary" component="p">
-          Something here to give the footer a purpose!
+          Isso foi difícil pra caramba kkkkk
         </Typography>
         <Copyright />
       </footer>
