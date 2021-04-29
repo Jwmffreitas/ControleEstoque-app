21c76e12fef12a6415a48b5bd06459ab7d688ff7
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-04-29 08:27:45 -0300

    Tirando código comentado

--- src/routes.js
+++ src/routes.js
@@ -6,11 +6,6 @@ const Produto = require('./controllers/produto-controller')
 
 routes.get('/', Produto.index)
 
-
-/*routes.get('/', function(req, res) {
-    res.json({message: "Hello World"})
-})*/
-
 // Rotas de produtos
 routes.post('/api/produtos', Produto.create)
 routes.get('/api/produtos', Produto.index)
