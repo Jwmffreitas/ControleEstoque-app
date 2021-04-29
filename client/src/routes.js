2fd8a0bc8719e42129d30a197b1156ab8d4c518b
João Vitor Freitas <65046228+Jwmffreitas@users.noreply.github.com>
2021-04-29 08:05:37 -0300

    Ajustando rotas inutilizadas

--- client/src/routes.js
+++ client/src/routes.js
@@ -3,14 +3,12 @@ import React from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
 //IMPORTS ADMIN
-import Dashboard from './pages/admin/dashboard'
 
 import Produtos from './pages/admin/produtos'
 import ProdutosEditar from './pages/admin/produtos/produtos-editar'
 import ProdutosCadastrar from './pages/admin/produtos/produtos-cadastrar'
 
 //IMPORTS CLIENT
-import Home from './pages/client/home'
 import ProdutosDetalhes from './pages/client/produtos/produtos-details'
 
 export default function Routes() {
@@ -18,11 +16,10 @@ export default function Routes() {
         <BrowserRouter>
             <Switch>
                 {/*Rota client*/}
-                <Route path="/" exact component={Home}/>
+                <Route path="/" exact component={Produtos}/>
                 <Route path="/produtos/:idProduto" exact component={ProdutosDetalhes}/>
 
                 {/*Rota admin*/}
-                <Route path="/admin" exact component={Dashboard}/>
                 <Route path="/admin/produtos" exact component={Produtos}/>
                 <Route path="/admin/produtos/cadastrar" exact component={ProdutosCadastrar}/>
                 <Route path="/admin/produtos/editar/:idProduto" exact component={ProdutosEditar}/>
